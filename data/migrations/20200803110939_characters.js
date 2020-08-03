87b68fe6d07ee120253565962fd8876dbc02ec9c
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-03 16:45:35 -0600

    added user_id to characters table

--- data/migrations/20200803110939_characters.js
+++ data/migrations/20200803110939_characters.js
@@ -2,6 +2,13 @@
 exports.up = function(knex) {
     return knex.schema.createTable('characters', tbl => {
         tbl.increments()
+        tbl.integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
         tbl.string('name')
             .notNullable()
         tbl.integer('level')
