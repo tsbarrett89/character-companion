c14374ad724332388025ac932dc0f082f048fc9a
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-03 16:08:18 -0600

    remove user endpoint

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -16,4 +16,27 @@ router.get('/:username', (req, res) => {
         })
 })
 
+router.delete('/:username', (req, res) => {
+    const username = req.params.username
+
+    users.findBy({ username })
+        .first()
+        .then(user => {
+            if(user){
+                users.removeUser(user.id)
+                    .then(deleted => {
+                        res.status(201).json({ user_id: user.id, username: user.username})
+                    })
+                    .catch(err => {
+                        res.status(409).json({ message: "Unable to remove user."})
+                    })
+            } else {
+                res.status(404).json({ message: "Could not find user."})
+            }
+        })
+        .catch(err => {
+            res.status(404).json({ message: "Could not find user." })
+        })
+})
+
 module.exports = router
