381a5668d2d46032a87e850f421afd593776affc
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-03 14:51:20 -0600

    login endpoint

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -17,9 +17,31 @@ router.post('/register', (req, res) => {
 
                 res.status(201).json({ user_id: userId[0], token })
             })
+            .catch(err => {
+                res.status(409).json({ message: "unable to create user" })
+            })
     } else {
         res.status(400).json({ message: 'username and password are required' })
     }
+})
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body
+
+    users.findBy(username)
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)){
+                const token = createToken(user.id)
+
+                res.status(200).json({ user_id: user.id, token })
+            } else {
+                res.status(401).json({ message: "Invalid credentials" })
+            }
+        })
+        .catch(err => {
+            res.status(403).json({ message: "user not found" })
+        })
 })
 
 module.exports = router
