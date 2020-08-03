b204e3adb160a1126f7e1c9e12f84ba97168d522
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-03 14:24:34 -0600

    register endpoint

---
+++ auth/authRouter.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcryptjs')
+const express = require('express')
+const users = require('./authModel.js')
+const generateToken = require('./token')
+
+const router = express.Router()
+
+router.post('/register', (req, res) => {
+    const creds = req.body
+
+    if (creds.username && creds.password){
+        const hash = bcrypt.hashSync(creds.password, 12)
+        creds.password = hash
+        users.add(creds)
+            .then(userId => {
+                const token = generateToken(userId)
+
+                res.status(201).json({ user_id: userId, token })
+            })
+    } else {
+        res.status(400).json({ message: 'username and password are required' })
+    }
+})
