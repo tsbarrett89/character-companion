5c7d491d173de7cfad53b9920658f7b9d98f5819
tsbarrett89 <tsbarrett89@gmail.com>
2020-08-03 21:00:37 -0600

    routers for armor to weapon

--- api/server.js
+++ api/server.js
@@ -4,6 +4,13 @@ const cors = require('cors')
 const authRouter = require('../auth/authRouter')
 const usersRouter = require('../users/usersRouter')
 const charactersRouter = require('../characters/charactersRouter')
+const armorRouter = require('../characters/armor/armorRouter')
+const equipmentRouter = require('../characters/equipment/equipmentRouter')
+const featsRouter = require('../characters/feats/featsRouter')
+const languagesRouter = require('../characters/languages/languagesRouter')
+const skillsRouter = require('../characters/skills/skillsRouter')
+const toolsRouter = require('../characters/tools/toolsRouter')
+const weaponRouter = require('../characters/weapon/weaponRouter')
 
 const server = express()
 
@@ -17,5 +24,12 @@ server.get('/', (req, res) => {
 server.use('/auth', authRouter)
 server.use('/users', usersRouter)
 server.use('/characters', charactersRouter)
+server.use('/armor', armorRouter)
+server.use('/equipment', equipmentRouter)
+server.use('/feats', featsRouter)
+server.use('/languages', languagesRouter)
+server.use('/skils', skillsRouter)
+server.use('/tools', toolsRouter)
+server.use('/weapon', weaponRouter)
 
 module.exports = server
